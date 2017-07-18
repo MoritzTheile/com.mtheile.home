6299dc1471c744d790ec2e32b00554a50d449327
MoritzTheile <github.com@moritztheile.de>
2017-07-18 14:07:51 +0200

    added minimum teamtable content

--- src/main/webapp/app/teamtable/teamtable.component.ts
+++ src/main/webapp/app/teamtable/teamtable.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 import { Router } from '@angular/router';
@@ -9,7 +9,8 @@ import { Account, LoginModalService, Principal } from '../shared';
     selector: 'jhi-teamtable',
     templateUrl: './teamtable.component.html',
     styleUrls: [
-        'teamtable.css'
+        'teamtable.css',
+        '../../content/css/mtheile.css'
     ]
 })
 export class TeamtableComponent implements OnInit {
